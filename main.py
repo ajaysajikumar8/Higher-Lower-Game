93e1fbe80f3ba027a9aa5226a4c4161112f3359c
Ajay <ajaysajikumar8@gmail.com>
2022-01-18 22:48:11 +0530

    Changes updated

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ import random
 def choose():
     return random.choice(data)
 
-#make a function to print the description
-def description(individual):
-    name = individual["name"]
-    description = individual["description"]
-    country = individual["country"]
-    print(f"{name}, a {description}, from {country}")
+#make an individual description function for both A and B.
+def CompareA(a):
+    print(f"Compare A: {a['name']}, a {a['description']}, from {a['country']}")
+
+def AgainstB(b):
+    print(f"Against B: {b['name']}, a {b['description']}, from {b['country']}")
+
 
 #compare the follower count of A and B (make a function)
 def compare(a,b):
@@ -29,36 +30,35 @@ def game():
     print(logo)
     a = choose()
     
-
     #to keep track of the score. 
     score = 0
     #TODO put a flag here and make a while loop from here
     game_over = False
     while not game_over:
-        description(a)
+        CompareA(a)
         print(vs)
         b = choose()
-        description(b)
+        AgainstB(b)
         greater_followers = compare(a,b)
         
         #Ask the user for input. that is who has the most number of followers
         user_input = input("Who has more followers? 'A' or 'B': ").lower()
         if user_input == greater_followers:
+            #if the user guesses the number of followers right, then raise the score. show the score.
             score += 1
             print(f"You got it right.")
             print(f"Score : {score}")
             a = b
         else:
+            #if the user guesses the wrong answer, tell the user that they are wrong and print the score. then the game ends
             print(f"You are wrong. You lose. Your total score is {score}")
             game_over = True
         
-        #ask the user if he wants to play again?
+    #ask the user if he wants to play again?
     choice = input("Do you want to play again?. Type 'y' for yes and 'n' for no. ").lower()
     if choice == "y":
         game()
-#if the user guesses the number of followers right, then raise the score. show the score.
 
-#if the user guesses the wrong answer, tell the user that they are wrong and print the score. then the game ends
 
 game()
 
